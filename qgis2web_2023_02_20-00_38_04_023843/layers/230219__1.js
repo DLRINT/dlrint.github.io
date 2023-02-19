614947b3e92655f0c3d7a0074c522e8e81e15df6
DLRINT <daler_8788@mail.ru>
2023-02-20 00:47:34 +0500

    klol

--- qgis2web_2023_02_20-00_38_04_023843/layers/230219__1.js
+++
@@ -1,1 +0,0 @@
-var json_230219__1 = {"type":"FeatureCollection","name":"230219__1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"Autogenerate"},"geometry":{"type":"Point","coordinates":[69.00385420471541,39.911609927831016]}}]}
